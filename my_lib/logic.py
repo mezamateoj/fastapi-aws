f0a099b408e7d5077df02e41f46668422d92e8dc
Mateo Meza <mezamateoj@gmail.com>
2023-11-14 13:19:43 -0300

    Add new search function and cli changes

--- my_lib/logic.py
+++ my_lib/logic.py
@@ -2,10 +2,20 @@ import wikipedia
 
 
 def wiki(name="Apple III", length=1):
-    "Wikipedia fetcher function"
+    """Wikipedia fetcher function"""
 
     try:
         return wikipedia.summary(name, length)
     except Exception as e:
         print(e)
         return "ERROR: Could not get the info..."
+
+def search_wiki(name):
+    """Search Wikipedia articles by title, name"""
+    
+    try:
+        return wikipedia.search(name)
+    except Exception as e:
+        print(e)
+        return "ERROR: Could not get the info..."
+
